817a99d01dde24a6878338ab8d655273c58afd40
brayangarces <brayangarces@192.168.0.10>
2019-04-21 01:26:18 -0500

    RBT test has been added

--- src/test/RedBlackTreeTest.java
+++ src/test/RedBlackTreeTest.java
@@ -5,10 +5,38 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import dataStructures.*;
 class RedBlackTreeTest{
+private RedBlackTree<Integer> tree;
+	
+	void stage1() {
+		tree= new RedBlackTree<Integer>();
+		tree.add(8);
+		tree.add(4);
+		tree.add(34);
+		tree.add(98);
+		tree.add(233);
+		tree.add(1450);
+		tree.add(0);
+		tree.add(734);
+		tree.add(1200);
+		tree.add(1000);
+		tree.add(98765);
+		tree.add(98754);
+	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void searchTest() {
+		stage1();
+		
+		assertEquals(233, (int)tree.search(233).getID());
+		assertEquals(734, (int)tree.search(734).getID());
 	}
+
+	@Test
+	public void addTest() {
+		stage1();
+		assertEquals(12, tree.size());
+	}
+
+	
 
 }
